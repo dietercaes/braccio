528bb1568a2678f8e95018d664020ddcba7c8cb5
dietercaes <47667377+dietercaes@users.noreply.github.com>
2020-09-23 21:51:02 +0200

    Update BracciosimpleMovements.ino

--- BracciosimpleMovements/BracciosimpleMovements.ino
+++ BracciosimpleMovements/BracciosimpleMovements.ino
@@ -21,6 +21,7 @@ Servo gripper;
 
 
 void moveToPosition(unsigned int x, unsigned int z){
+  // calculating the length of the vector from 0, 195 to target point x, z using 
   double lengthBCvector = sqrt((XVALUE - x) * (XVALUE - x) + (ZVALUE - z) * (ZVALUE - z));
 
   // angles
@@ -33,10 +34,13 @@ void moveToPosition(unsigned int x, unsigned int z){
   int b = B;
   int c = C;
 
+  // calculating the angles of the triangle consisting of vectors B, C and BC using the lengths of the sides.
+  // # TODO: check sum to be 180°, fix calculations using proper Arduino code.
   angleA = acos(((b*b) + (c*c) - (a*a))/(2 * b * c));
   angleB = acos((a*a + c*c - b*b)/(2 * a * c));
   angleC = acos((a*a + b*b - c*c)/(2 * a * b));  
-  
+
+  // move Braccio to correct position.
   Braccio.ServoMovement(20,   90, 90, round(angleA), round(angleB), 90, 10);
 }
 
